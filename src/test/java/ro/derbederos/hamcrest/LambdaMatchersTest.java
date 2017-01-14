724a2b6478d84a2bbe8ed33715dd88c37397316b
Claudiu Soroiu <csoroiu@gmail.com>
2017-01-14 13:32:29 +0200

    version 0.13

--- src/test/java/ro/derbederos/hamcrest/LambdaMatchersTest.java
+++ src/test/java/ro/derbederos/hamcrest/LambdaMatchersTest.java
@@ -65,11 +65,13 @@ public class LambdaMatchersTest {
         // retrolambda creates an access method for a private instance method reference
         assertDescription(anyOf(
                 equalTo("a Person having `LambdaMatchersTest::getPersonName` a string starting with \"B\""),
-                equalTo("a Person having `(String)LambdaMatchersTest::access$lambda$1` a string starting with \"B\"")),
+                equalTo("a Person having `(String)LambdaMatchersTest::access$lambda$1` a string starting with \"B\""),
+                equalTo("a Person having `(String)LambdaMatchersTest::access$lambda$simpleTestInstanceObjectMethodReferenceDescription$1` a string starting with \"B\"")),
                 mapMatcher);
         assertMismatchDescription(anyOf(
                 equalTo("`LambdaMatchersTest::getPersonName` was \"Alice\""),
-                equalTo("`(String)LambdaMatchersTest::access$lambda$1` was \"Alice\"")),
+                equalTo("`(String)LambdaMatchersTest::access$lambda$1` was \"Alice\""),
+                equalTo("`(String)LambdaMatchersTest::access$lambda$simpleTestInstanceObjectMethodReferenceDescription$1` was \"Alice\"")),
                 new Person("Alice", 21), mapMatcher);
     }
 
