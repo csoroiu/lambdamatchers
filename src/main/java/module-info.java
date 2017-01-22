5006068c6fc67e7de7bcad44a30a0cf38c8edef7
Claudiu Soroiu <csoroiu@gmail.com>
2017-01-22 15:35:45 +0200

    [build] added module-info.java and prepare building with jdk9

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ro.derbederos.hamcrest {
+    exports ro.derbederos.hamcrest;
+
+    requires java.base;
+    requires jdk.unsupported;
+    requires hamcrest.all;
+    requires streamsupport;
+}
